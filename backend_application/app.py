e86f95da25dcb6265b2d46359596cf4adcb4cb0d
Yeliang-Radish <yeliang.shou@gmail.com>
2019-09-14 14:26:58 -0400

    Added some more routes

--- backend_application/app.py
+++ backend_application/app.py
@@ -5,9 +5,44 @@ import json
 app = Flask(__name__)
 CORS(app)
 
+
 @app.route('/')
 def index():
-  return 'Hello world'
+    return 'Hello world'
+
+
+@app.route('/home')
+def home():
+    return 'my home'
+
+
+@app.route('/dashboard')
+def dashboard():
+    return "yeetboard"
+
+
+@app.route('/')
+# For registering a user
+@app.route('/user', methods=['POST'])
+def create_user():
+    print(request.get_json())
+    return "got user"
+
+
+# Registering a new group
+@app.route('/group', methods=['POST'])
+def create_group():
+    print(request.get_json())
+    return "Creating group"
+
+# Assigning a user to a new group
+
+
+@app.route('/group', methods=['POST'])
+def assign_user_to_group():
+    print(request.get_json())
+    return "Assigning user to group"
+
 
 if __name__ == '__main__':
-  app.run(debug=True)
+    app.run(debug=True)
