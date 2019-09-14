256d5785a7fef3091acfaef17c364d6a166c83cd
Yeliang-Radish <yeliang.shou@gmail.com>
2019-09-14 14:34:24 -0400

    modify route

--- backend_application/app.py
+++ backend_application/app.py
@@ -8,41 +8,34 @@ CORS(app)
 
 @app.route('/')
 def index():
-    return 'Hello world'
-
+  return 'Hello world'
 
 @app.route('/home')
 def home():
-    return 'my home'
-
+  return 'my home'
 
 @app.route('/dashboard')
 def dashboard():
-    return "yeetboard"
+  return "yeetboard"
 
-
-@app.route('/')
 # For registering a user
 @app.route('/user', methods=['POST'])
 def create_user():
-    print(request.get_json())
-    return "got user"
-
+  print(request.get_json())
+  return "got user"
 
 # Registering a new group
 @app.route('/group', methods=['POST'])
 def create_group():
-    print(request.get_json())
-    return "Creating group"
+  print(request.get_json())
+  return "Creating group"
 
 # Assigning a user to a new group
-
-
 @app.route('/group', methods=['POST'])
 def assign_user_to_group():
-    print(request.get_json())
-    return "Assigning user to group"
+  print(request.get_json())
+  return "Assigning user to group"
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run(debug=True)
